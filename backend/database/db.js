cea55a79d287aa9130dcdfbf30a5d8dc5bf87fb0
Zaineb-fad <zainebfad2021@gmail.com>
2021-11-12 18:34:00 +0100

    first commit

---
+++ backend/database/db.js
@@ -0,0 +1,3 @@
+module.exports = {
+   db: 'mongodb://zaineb:95031285@cluster0-shard-00-00.q3vei.mongodb.net:27017,cluster0-shard-00-01.q3vei.mongodb.net:27017,cluster0-shard-00-02.q3vei.mongodb.net:27017/upload?ssl=true&replicaSet=atlas-9ts4y7-shard-0&authSource=admin&retryWrites=true&w=majority'
+};
